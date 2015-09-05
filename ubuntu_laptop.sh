9aaedd7648e2d2d5744babf4b9dd6bc11f18baa1
magic2k <magic2ktech@gmail.com>
2015-09-05 22:31:14 +0300

    init

---
+++ ubuntu_laptop.sh
@@ -0,0 +1,47 @@
+#/bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install git vim nodejs fonts-inconsolata fonts-ricty-diminished autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev libffi-dev curl imagemagick ctags sqlite libsqlite3-dev libpq-dev
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install 2.2.3
+rbenv global 2.2.3
+rbenv rehash
+
+curl -Lo- https://bit.ly/janus-bootstrap | bash
+mkdir ~/.janus
+cd ~/.janus && git clone https://github.com/bling/vim-airline.git
+cd ~/.janus && git clone https://github.com/flazz/vim-colorschemes.git
+cd ~/.janus && git clone https://github.com/jiangmiao/auto-pairs.git
+cd ~/.janus && git clone https://github.com/tpope/vim-ragtag.git
+cp -R ~/.janus/vim-colorschemes/colors ~/.vim/
+
+cp ~/.vimrc ~/.vimrc.vsop
+cat >> ~/.vimrc <<EOL
+" my custom settings
+set nocompatible
+set t_Co=256
+
+" colo guardian
+colo greens
+" colo elda
+colo bluedrake
+" colo radicalgoodspeed
+colo Chasing_Logic
+
+" let g:airline_theme='luna'
+let g:airline_theme='dark'
+" let g:airline_powerline_fonts=1
+set laststatus=2
+EOL
+
+gem install bundler
+gem install pry
+
+sudo apt-get -y install postgresql-9.4
+
+#run 'bundle update' in rails projects folders.
+
